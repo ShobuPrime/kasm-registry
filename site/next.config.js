5d1083dc5940a6ccab0e8da402c9310d25e617a5
3D <anthony.dardano@outlook.com>
2024-05-15 00:27:39 -0400

    Init `next.config.js` for Kasm Registry

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'ShobuSphere',
+    description: 'ShobuPrime\'s List of Kasm Workspaces.',
+    icon: 'https://shobuprime.github.io/kasm-registry/1.0/icon.png',
+    listUrl: 'https://shobuprime.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/ShobuPrime/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
